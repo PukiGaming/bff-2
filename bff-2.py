3abb6eec0fb5f77c29c6181a32cfd3ef7a7af595
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2021-10-10 01:20:27 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -3,7 +3,7 @@ import os,sys
 
 print("\n • Sorry, script telah di hapus. ")
 print(" • Tunggu Update terbaru, tanggal 13 Oktober 2021.")
-print(" • Untuk update bff-2 pada tangga 13 Oktober silahkan ketik :\n ")
+print(" • Untuk update bff-2 pada tangga 13 Oktober\n • silahkan ketik :\n ")
 print(" $ rm -rf bff-2")
 print(" $ git clone https://github.com/Mark-Zuck/bff-2 ")
 print(" $ cd bff-2 ")
