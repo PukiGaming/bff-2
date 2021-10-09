e67959e34d3a1314cfefa25ab78108f08328dba8
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2021-10-10 01:21:00 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import os,sys
 
-print("\n • Sorry, script telah di hapus. ")
+print("\n\x1b[1;92m • Sorry, script telah di hapus. ")
 print(" • Tunggu Update terbaru, tanggal 13 Oktober 2021.")
 print(" • Untuk update bff-2 pada tangga 13 Oktober\n • silahkan ketik :\n ")
 print(" $ rm -rf bff-2")
@@ -11,4 +11,4 @@ print(" $ pip2 install -r bff.txt ")
 print(" $ python2 bff-2.txt\n ")
 print(" • update tidak menggunakan gitt pul ")
 print(" • karna saya menghapus file bff-2 ")
-print(" •  Terima kasih :)\n ")
+print(" • Terima kasih :)\n ")
